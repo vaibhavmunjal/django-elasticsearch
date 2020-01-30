a0c758e8e28160293afd8b5036c52d6bf2ce4a69
vaibhavmunjal <vaibhavmunjal17@gmail.com>
2020-01-30 14:53:39 +0530

    search voa elasticsearch-dsl

--- search/app/views.py
+++ search/app/views.py
@@ -3,11 +3,18 @@ from django.http import HttpResponse
 from django.db.models import Q
 # Create your views here.
 
+
+from elasticsearch_dsl import Search as DSL_Search
+
+
 from .models import Search
 from .documents import SearchDocument
 
 
 def search_view(request):
+	"""
+	Search by Django filter
+	"""
 	if request.method == 'GET':
 		matched_search = Search.objects.all()
 
@@ -25,10 +32,36 @@ def search_view(request):
 	return HttpResponse('<h3>Only Get method Is Allowed</h3>')
 
 
-def elastic_search_view(request):
+def django_elastic_search_view(request):
+	"""
+	Search by django-elasticsearch-dsl
+	"""
 	if request.method == 'GET':
 
 		matched_search = SearchDocument.search()
+		q = request.GET.get('q')
+		description = request.GET.get('description')
+
+		if q:
+			matched_search = matched_search.query('match', name=q)
+
+		if description:
+			description = f'*{description}*'
+			matched_search = matched_search.filter('wildcard', description=description)
+
+		return render(request, 'search/search.html', context={'matched_search':matched_search})
+
+	return HttpResponse('<h3>Only Get method Is Allowed</h3>')
+
+
+def elastic_search_view(request):
+	"""
+	Search by elasticsearch-dsl
+	"""
+	if request.method == 'GET':
+
+		matched_search = DSL_Search(index="search_result")
+
 		q = request.GET.get('q')
 		description = request.GET.get('description')
 
